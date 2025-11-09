17b33c2e4b2a03a67e26cf0018c9de6d82aa6570
NikolVasileva <nikol.vasileva9@gmail.com>
2025-11-09 15:02:34 +0200

    Get dynamic post content

--- src/components/PostElement.jsx
+++ src/components/PostElement.jsx
@@ -1,16 +1,16 @@
 import styles from "./PostElement.module.css";
 
-export default function PostElement() {
+export default function PostElement({
+    title,
+    body
+}) {
     return (
         <article className={styles.post}>
             <h3 className={styles.postTitle}>
-                sunt aut facere repellat provident occaecati excepturi optio reprehenderit
+                {title}
             </h3>
             <p className={styles.postBody}>
-                quia et suscipit
-                suscipit recusandae consequuntur expedita et cum
-                reprehenderit molestiae ut ut quas totam
-                nostrum rerum est autem sunt rem eveniet architecto
+                {body}
             </p>
         </article>
     );
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import PostElement from "./PostElement.jsx";
 
 export default function Posts() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => {
+            setPosts(Object.values(data))
+        })
+    }, [])
+
     return(
         <div>
-            <PostElement />
+            {posts.map(post => <PostElement 
+            key={post.id}
+            id={post.id}
+            title={post.title}
+            body={post.body}
+            />)}
         </div>
     )
 }
